36a6d31ac5004203b9c5e1ccc9c1e98c1ce55fbe
Aquilesxd <fernandoalanfillmann@gmail.com>
2024-02-02 14:24:34 -0300

    feat: finalização do visual do header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
-import { Typography } from "@mui/material";
+import { Box, Fab, TextField, Typography } from "@mui/material";
+import RestartAltRoundedIcon from '@mui/icons-material/RestartAltRounded';
 
-import { HeaderBox } from "./HeaderStyles";
+import { HeaderBox, HeaderDateBox } from "./HeaderStyles";
 
 export default function Header() {
 
+  const temporaryDate = "2017-05-24";
 
   return (
     <HeaderBox 
@@ -19,6 +21,46 @@ export default function Header() {
       >
         Backstage Organizer
       </Typography>
+      {
+        temporaryDate &&
+        <Box
+          display="flex"
+          flexDirection="column"
+        >
+          <HeaderDateBox>
+            <Box display="flex" width="100%">
+              <TextField
+                disabled={true}
+                size="small"
+                label="Início da Semana"
+                type="date"
+                value={temporaryDate}
+              />
+              <Box mx={1}>
+                <Fab 
+                  color="warning" 
+                  aria-label="limpar"
+                  size="small"
+                  onClick={()=>{
+                    //Abre modal para configurar nova semana
+                    // modal vai pedir numero da semana somente para prencher
+                  }}
+                >
+                  <RestartAltRoundedIcon fontSize="large" />
+                </Fab>
+              </Box>
+            </Box>
+            <Typography 
+              fontFamily="sans-serif"
+              mt={1}
+              variant="body1"
+              maxWidth="260px"
+            >
+              Você ainda tem 4 dias para concluir as tarefas dessa semana.
+            </Typography>             
+          </HeaderDateBox>     
+        </Box>
+      }
     </HeaderBox>
   )
 }
--- src/components/HeaderStyles.ts
+++ src/components/HeaderStyles.ts
@@ -8,9 +8,22 @@ import { defaultTheme } from "../theme";
 export const HeaderBox = styled(Box)({
   width: "100%",
   display: "flex",
+  justifyContent: "space-between",
   alignItems: "center",
-  padding: defaultTheme.spacing(2),
+  padding: defaultTheme.spacing(1),
+  paddingRight: defaultTheme.spacing(2),
+  paddingLeft: defaultTheme.spacing(2),
+  py: defaultTheme.spacing(2),
   borderBottom: "4px solid #00A99D",
   boxShadow: "1px 1px 1px rgba(0, 0, 0, 0.6)",
   backgroundImage: `url(${fundoPapelInfinitoIMG})`
+});
+
+export const HeaderDateBox = styled(Box)({
+  textAlign: "left",
+  backgroundColor: "#ffffff",
+  borderRadius: "6px",
+  boxShadow: "2px 1px 1px rgba(0, 0, 0, 0.5)",
+  padding: defaultTheme.spacing(1),
+  paddingTop: defaultTheme.spacing(1.5)
 });
