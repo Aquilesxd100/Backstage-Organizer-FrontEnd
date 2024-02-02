5530aa0507f11f3ea323f292dd9eee4f46d159b0
Aquilesxd <fernandoalanfillmann@gmail.com>
2024-02-02 17:23:53 -0300

    feat: desenvolvimento inicial de conversao do arquivo PDF

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,6 @@
 
 import { WeeklyTasksData } from "../types/types";
+import getPDFData from "../utils/pdfExtraction/pdfDataExtraction";
 import { ContentBox } from "./ContentStyles";
 
 interface IContentProps {
@@ -9,10 +10,12 @@ interface IContentProps {
 export default function Content({
   weeklyTasksData
 }: IContentProps) {
-  
+
   return (
     <ContentBox elevation={5}> 
-      
+      <button onClick={async ()=>{ await getPDFData()}}>
+        GERAR DADOS
+      </button>
     </ContentBox>
   )
 }
