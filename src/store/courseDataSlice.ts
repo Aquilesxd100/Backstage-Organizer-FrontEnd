6204cde2127a808b4b5029d639889e128096c877
Aquilesxd <fernandoalanfillmann@gmail.com>
2024-02-03 02:32:55 -0300

    feat: reducer de set da semana por numero da mesma

--- src/store/courseDataSlice.ts
+++ src/store/courseDataSlice.ts
@@ -23,9 +23,19 @@ export const courseDataSlice = createSlice({
       const localStorageData = localStorage.getItem("courseSavedWeek");
       if (localStorageData)
         state.currentWeekData = JSON.parse(localStorageData)
+    },
+    setWeekDataByNumber: (state, action: PayloadAction<number>) => {
+      if (state.courseDataBase?.weeklyTasksData[action.payload]) {
+        const currentDateInPTBRFormat = (new Date()).toLocaleDateString('pt-BR');
+
+        state.currentWeekData = {
+          ...state.courseDataBase?.weeklyTasksData[action.payload],
+          startDate: currentDateInPTBRFormat
+        };
+      }
     }
   }
 })
 
-export const { fillCourseDataBase, tryToGetSavedWeekData } = courseDataSlice.actions;
+export const { fillCourseDataBase, tryToGetSavedWeekData, setWeekDataByNumber } = courseDataSlice.actions;
 export default courseDataSlice.reducer;
