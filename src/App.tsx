c1d6cd5ededf983e5abfe9a85f5ae6db6c188de4
Aquilesxd <fernandoalanfillmann@gmail.com>
2024-02-02 22:17:26 -0300

    revert: remocao de mock

--- src/App.tsx
+++ src/App.tsx
@@ -7,34 +7,6 @@ import "./css/styles.css";
 import { useState } from "react";
 import { WeeklyTasksData } from "./types/types";
 
-const weeklyTasksDataMock : WeeklyTasksData = {
-  weekNumber: 1,
-  weekTitle: "I will study more",
-  startDate: "2017-05-24",
-  tasks: [
-    {
-      isDone: false,
-      title: " titulo 1",
-      description: "teste descricao"
-    },
-    {
-      isDone: false,
-      title: " titulo 2",
-      description: "teste descricao2"
-    },
-    {
-      isDone: false,
-      title: " titulo 3",
-      description: "teste descricao3"
-    },
-    {
-      isDone: false,
-      title: " titulo 4",
-      description: "teste descricao4"
-    },
-  ]
-}
-
 function App() {
   const [weeklyTasksData, setWeeklyTasksData] = 
     useState<WeeklyTasksData>(weeklyTasksDataMock);
