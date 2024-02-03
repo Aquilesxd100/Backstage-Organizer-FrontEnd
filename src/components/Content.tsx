3b4ad4a492badd069d06ae7c440293514778faf5
Aquilesxd <fernandoalanfillmann@gmail.com>
2024-02-03 00:07:21 -0300

    refactor: remocao de props recebidas pelo content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,16 +1,11 @@
 import { WeeklyTasksData } from "../types/types";
 import { ContentBox } from "./ContentStyles";
 
-interface IContentProps {
-  weeklyTasksData: WeeklyTasksData
-}
-
-export default function Content({
-  weeklyTasksData
-}: IContentProps) {
+export default function Content() {
 
   return (
     <ContentBox elevation={5}> 
+      
     </ContentBox>
   )
 }
