4101721ea7bd127d07fcd08bd5c456bb3708d900
Aquilesxd <fernandoalanfillmann@gmail.com>
2024-02-03 18:39:30 -0300

    feat: retoques stilizacao box de tarefas e conteudo

--- src/components/ContentStyles.ts
+++ src/components/ContentStyles.ts
@@ -6,7 +6,24 @@ import { defaultTheme } from "../theme";
 
 export const ContentBox = styled(Paper)({
   width: `calc(100% - ${defaultTheme.spacing(3)})`,
-  minHeight: `calc(100vh - 120px - ${defaultTheme.spacing(3)})`,
+  minHeight: `calc(100vh - 180px - ${defaultTheme.spacing(3)})`,
   padding: defaultTheme.spacing(1),
   margin: defaultTheme.spacing(1.5),
-})
+});
+
+export const TaskBox = styled(Box)({
+  cursor: "pointer",
+  userSelect: "none",
+  backgroundColor: "#FFFFFF",
+  overflow: "hidden",
+  boxShadow: "2px 2px 1px rgba(0, 0, 0, 0.6)",
+  margin: defaultTheme.spacing(1),
+  width: "25%",
+  minWidth: "200px",
+  border: "3px solid #00A99D",
+  borderRadius: "8px",
+  "&:hover": {
+    filter: "brightness(0.93)",
+    transform: "scale(1.05)"
+  }
+});
